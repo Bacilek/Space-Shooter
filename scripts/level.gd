0c76195f55b9b53404bb323a2ff9cb27b71550bf
Marek Doupovec <DoupovecMarek@seznam.cz>
2025-10-18 02:11:32 +0200

    meteors spawn with random properties, layers & masks solved

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+# 1. load scene
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 
-func _on_timer_timeout() -> void:
-	print("meteor")
+func _on_meteor_timer_timeout() -> void:
+	# 2. create instance of the loaded scene 
+	var meteor = meteor_scene.instantiate()
+	
+	# 3. attach the node/instance to the scene
+	$Meteors.add_child(meteor)
